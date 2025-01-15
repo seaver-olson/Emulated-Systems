bdcbeddd5be696ec75a406c9f94b977a8f24225d
seaver-olson <sjolson007@gmail.com>
2025-01-15 15:02:06 -0600

    have to push to ubuntu for sdl

--- chip8/display.c
+++ chip8/display.c
@@ -2,33 +2,4 @@
 
 uint8_t display[DISPLAY_WIDTH * DISPLAY_HEIGHT];
 
-void display_clear(){
-    for (int i = 0; i < DISPLAY_WIDTH * DISPLAY_HEIGHT; i++){
-        display[i] = 0;
-    }
-}
 
-void display_draw(uint8_t x, uint8_t y, uint8_t val){
-    display[x + y * DISPLAY_WIDTH] = val;
-}
-
-void display_print(){
-    for (int i = 0; i < DISPLAY_HEIGHT; i++){
-        for (int j = 0; j < DISPLAY_WIDTH; j++){
-            if (display[j + i * DISPLAY_WIDTH] == 0) printf(" ");
-            else printf("X");
-        }
-        printf("\n");
-    }
-}
-
-void display_print_block(uint16_t addr, int size){
-    for (int i = 0; i < size; i++){
-        printf("%02x ", display[addr + i]);
-    }
-    printf("\n");
-}
-
-void display_write_block(uint16_t addr, uint8_t val){
-    display[addr] = val;
-}
--- chip8/display.h
+++ chip8/display.h
@@ -1,5 +1,6 @@
 //64 by 32 display for the chip8
 #include "mem.h"
+#include <SDL2/SDL.h>
 
 #define DISPLAY_WIDTH 64
 #define DISPLAY_HEIGHT 32
